205ea527c69b1688d63562685e34afc0d0248e37
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-12-24 10:47:58 +0900

    Introducing superman, a talk-to ai agent
    I find qwen2.5:0.5b to be the best opensource model out there due to small size & great quality.
    
    However, the chinese origin can come off as hard to scale due to the censorship nature in it.
    
    The next best option is llama3.2:1b, but I find it too big for a RPi.
    
    Which should I pick?

--- tools/superman.py
+++ tools/superman.py
@@ -1,21 +1,35 @@
+from ollama import chat
+from ollama import ChatResponse
+
 class Superman:
-    def __init__(self, name="Clark Kent"):
-        self.name = name
-        self.powers = ["flight", "super strength", "x-ray vision", "heat vision"]
-        self.weakness = "kryptonite"
-        self.is_flying = False
+    def __init__(self, model = 'qwen2.5:0.5b', name="Clark Kent", personality="Always encouraging others towards greatness"):
+        self.introduction = f'''
+You are an honest person known as {name}, and your personality is {personality}.
+'''
+        self.model = model
+        
 
-    def fly(self):
-        self.is_flying = True
-        return f"{self.name} is now flying!"
+    def answer(self, prompt, web_context = '', file_context = '', memory_context = '', feeling_context = ''):
+        if web_context != '':
+            web_context = f'/nSupporting information from the web: {web_context}/n'
+        if file_context != '':
+            file_context = f'/nSupporting information from your files: {file_context}/n'
+        if memory_context != '':
+            memory_context = f'/nSupporting information from your memory: {memory_context}/n'
+        return self.talk(prompt = prompt, context = f'{web_context} {file_context} {memory_context} {feeling_context}')
+        
 
-    def use_power(self, power):
-        if power in self.powers:
-            return f"{self.name} uses {power}!"
-        return f"{self.name} doesn't have that power."
+    def talk(self, prompt, context=''):
+        stream = chat(
+            model=self.model,
+            messages=[{'role': 'user', 'content': f'{self.introduction} {context} {prompt}'}],
+            stream=True,
+            )
+        return stream
+        for chunk in stream:
+            print(chunk['message']['content'], end='', flush=True)
+        
+atlas = Superman(name='Princess Bubblegum', personality='A crazy and sexy female scientist in a candy kingdom')
 
-    def land(self):
-        if self.is_flying:
-            self.is_flying = False
-            return f"{self.name} has landed."
-        return f"{self.name} is already on the ground."
+for chunk in atlas.answer('Do you think engineers are cool?'):
+    print(chunk['message']['content'], end='', flush=True)
