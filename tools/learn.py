f78c8a832bc65c5bb9190b061f6fcab8b3f36bd7
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-12-24 09:07:55 +0900

    Adding in brave search for web search

--- tools/learn.py
+++ tools/learn.py
@@ -1,8 +1,47 @@
+from langchain_community.tools import BraveSearch
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+load_dotenv()
+
 class Web:
     def __init__(self):
+        #self.brave_tool = BraveSearch.from_api_key(api_key=os.environ["BRAVE_SEARCH_API"], search_kwargs={"count": 3})
+        self.headers = {
+        'Accept': 'application/json',
+        'Accept-Encoding': 'gzip',
+        'X-Subscription-Token': os.environ["BRAVE_SEARCH_API"]
+    }
+
+    def brave_search(self, query):
+        #result = self.brave_tool.run(query)
+        url = f"https://api.search.brave.com/res/v1/web/search?q={query}&summary=1"
+        search = requests.get(url, headers=self.headers)
+        search = search.json()
+        result = {}
+        result['query'] = query
+        result['snippets'] = []
+        for web_result in search['web']['results']:
+            if 'extra_snippets' in web_result:
+                for snippet in web_result['extra_snippets']:
+                    result['snippets'].append(snippet)
+        with open('search_result.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+        
+        return result
+    def facts(self, query):
+        result = self.brave_search(query)
+        context = 'Here are some facts and contextual information: '
+        for snippet in result['snippets']:
+            context = context + snippet + '\n'
+        
+        return context
+    def cache(self, query, result):
         pass
-    def search(self, query):
-        pass
-    def cache(self, query):
-        pass
+
+
+web = Web()
+print(web.facts("What is the capital of France?"))
 
