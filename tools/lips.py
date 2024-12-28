440f16923f6f0a6b4d3f98d4bdd0aea3192edbf7
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-12-27 20:48:46 -0500

    Tested out parler but conclusion is it's too compute intensive

--- tools/lips.py
+++ tools/lips.py
@@ -184,9 +184,31 @@ class Lips:
             self.audio_queue.put(None)
             self.print_queue.put(None)
 
-lip = Lips()
-from superman import Superman
+# lip = Lips()
+# from superman import Superman
 
-atlas = Superman(name='Princess Bubblegum', model = 'llama3.2:1b', personality='Crazy and sexy')
+# atlas = Superman(name='Princess Bubblegum', model = 'llama3.2:1b', personality='Crazy and sexy')
 
-lip.speak(atlas.answer('What is the meaning of life?'))
+# lip.speak(atlas.answer('What is the meaning of life?'))
+
+def parler():
+    import torch
+    from parler_tts import ParlerTTSForConditionalGeneration
+    from transformers import AutoTokenizer
+    import soundfile as sf
+
+    device = "cpu"
+
+    model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-mini-v1").to(device)
+    tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-mini-v1")
+
+    prompt = "Hey, how are you doing today?"
+    description = "A female speaker delivers a slightly expressive and animated speech with a moderate speed and pitch. The recording is of very high quality, with the speaker's voice sounding clear and very close up."
+
+    input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
+    prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+    generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
+    audio_arr = generation.cpu().numpy().squeeze()
+    sf.write("parler_tts_out.wav", audio_arr, model.config.sampling_rate)
+
